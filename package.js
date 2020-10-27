56035f44197b5e74a2d35bde9f1a6c056ff4efb3
Jan Küster <jkuester@uni-bremen.de>
2020-10-27 21:12:29 +0100

    published 1.0.0

--- package.js
+++ package.js
@@ -3,9 +3,9 @@ Package.describe({
   name: 'leaonline:theme',
   version: '1.0.0',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'This package adds the official leaonline bootstrap scss theme to a project.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'git@github.com:leaonline/theme.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -14,7 +14,7 @@ Package.describe({
 Package.onUse(function (api) {
   api.versionsFrom('1.6')
   api.use('ecmascript')
-  api.use('fourseven:scss')
+  api.use('fourseven:scss@4.12.0')
   api.addFiles('colors.scss', 'client', {isImport: true})
   api.addFiles('theme.scss', 'client', {isImport: true})
 })
