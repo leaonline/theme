8748259f59218e514c5c09c0da099a4398e98591
Jan Küster <jkuester@uni-bremen.de>
2022-09-26 08:50:54 +0200

    bump version to 1.1.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* eslint-env meteor */
 Package.describe({
   name: 'leaonline:theme',
-  version: '1.0.0',
+  version: '1.1.0',
   // Brief, one-line summary of the package.
   summary: 'This package adds the official leaonline bootstrap scss theme to a project.',
   // URL to the Git repository containing the source code for this package.
