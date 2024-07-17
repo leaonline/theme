d9bb433f766dbc4906a411e44ae8d38e4ea55e02
jankapunkt <jkuester@uni-bremen.de>
2024-07-17 12:26:35 +0200

    deps: upgrade to Meteor 3.0.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* eslint-env meteor */
 Package.describe({
   name: 'leaonline:theme',
-  version: '1.1.0',
+  version: '2.0.0',
   // Brief, one-line summary of the package.
   summary: 'This package adds the official leaonline bootstrap scss theme to a project.',
   // URL to the Git repository containing the source code for this package.
@@ -12,7 +12,7 @@ Package.describe({
 })
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.6')
+  api.versionsFrom(['3.0.1'])
   api.use('ecmascript')
   api.use('fourseven:scss@4.12.0')
   api.addFiles('colors.scss', 'client', { isImport: true })
